b93e34aa6e8896168fa3a7cf895d24a3dcdfb44d
KimSeHwan <ksh9241@naver.com>
2024-02-25 19:05:03 +0900

    Eureka 세팅

---
+++ src/test/java/io/sharing/server/eureka/CaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sharing.server.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
